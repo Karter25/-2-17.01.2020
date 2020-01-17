c4b8bfae4519cafdb9bf618d4886174f2cdade56
Бекжан Дания Низамикызы <@corp.itstep>
2020-01-17 18:01:57 +0600

    Добавьте файлы проекта.

---
+++ Урок 2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Урок_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int coins = 0;
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+            coins++;
+            label1.Text = Convert.ToString(coins);
+     
+            this.BackColor = Color.Pink;
+            this.Height = 400;
+            this.Width = 400;
+        }
+
+       
+    }
+}
---
+++ Урок 2/Template.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Урок_2
+{
+    public partial class Template : Form
+    {
+        public Template()
+        {
+            InitializeComponent();
+        }
+
+        //тип_функции имя_функции(аргументы)(){
+        // команды\код.....
+        //}
+
+            void sayHello(string name)
+        {
+
+            MessageBox.Show("alohomora " + name);
+        }
+            int matem(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            all += matem(Convert.ToInt32(textBox1.Text));
+            MessageBox.Show(Convert.ToString(all));
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
